9267e375e97bf375dc12aa3f630791b51b9b9fc2
Liviu Mateescu <liviumateescu@hotmail.com>
2017-12-26 02:02:26 +0200

    3

---
+++ PentruRPI/Db/MasTempContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PentruRPI.Db
+{
+    public class MasTempContext : DbContext
+    {
+        public DbSet<MasuratoriTemperatura> MasuratoriSenzor01 { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySql(@"Server=localhost;database=PentruRPI;uid=root;pwd=123456;");
+    }
+}
---
+++ PentruRPI/Db/MasuratoriTemperatura.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PentruRPI.Db
+{
+    public class MasuratoriTemperatura
+    {
+        public int SenzorId { get; set; }
+        public int Id { get; set; }
+        public double Valoare { get; set; }
+    }
+}
